f508fc7b5d0d60de9eff0d4fe193eb91de7dfc4c
LuisChen <leo5916267@gmail.com>
2019-10-06 20:02:57 +0800

    init react app

--- rollup-react-app/rollup.config.js
+++ rollup-react-app/rollup.config.js
@@ -1,21 +1,47 @@
 import resolve from 'rollup-plugin-node-resolve'
+import globals from 'rollup-plugin-node-globals'
+import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import { terser } from 'rollup-plugin-terser'
+import postcss from 'rollup-plugin-postcss'
+import livereload from 'rollup-plugin-livereload'
+import react from 'react'
+import reactDom from 'react-dom'
 
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
 const production = !process.env.ROLLUP_WATCH
 
+const babelConfig = {
+	babelrc: false,
+	presets: ['@babel/env', '@babel/preset-react'],
+	exclude: 'node_modules/**',
+	extensions: ['.ts', '.tsx', '.js', '.jsx', '.json']
+}
 export default {
 	input: 'src/index.js',
 	output: {
 		file: 'public/bundle.js',
-		format: 'umd', // immediately-invoked function expression — suitable for <script> tags
+		format: 'umd',
 		sourcemap: true
 	},
 	plugins: [
-		resolve(), // tells Rollup how to find date-fns in node_modules
-		commonjs(), // converts date-fns to ES modules
-		production && terser() // minify, but only in production
+		postcss({
+			plugins: [],
+			minimize: true,
+			sourceMap: 'inline'
+		}),
+		resolve(),
+		babel(babelConfig),
+		commonjs({
+			include: 'node_modules/**',
+			namedExports: {
+				react: Object.keys(react),
+				'react-dom': Object.keys(reactDom)
+			}
+		}),
+		globals(),
+		livereload(),
+		production && terser()
 	]
 }
--- rollup-react-app/src/components/App.js
+++ rollup-react-app/src/components/App.js
@@ -1,9 +1,13 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import './App.scss'
 const App = () => {
+	const [open, setOpen] = useState(false)
 	return (
-		<div>
-			<h1>App</h1>
+		<div className="background">
+			<h1>Hello React App</h1>
+			<p>you can try every thing</p>
+			<button onClick={() => setOpen(!open)}>try to click</button>
+			{open ? <p>hook opend</p> : <p>hook closed</p>}
 		</div>
 	)
 }
