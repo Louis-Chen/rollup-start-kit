653ca0c1e2a8536836906e742fa6a4b7a908c734
LuisChen <leo5916267@gmail.com>
2019-10-06 21:04:41 +0800

    add test command

--- rollup-react-app/src/components/App.js
+++ rollup-react-app/src/components/App.js
@@ -1,13 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+
 import './App.scss'
-const App = () => {
-	const [open, setOpen] = useState(false)
+
+function App() {
+	let [count, setCount] = useState(0)
+	let [name, setName] = useState(window.localStorage.getItem('name') || 'Brian')
+
+	let nameRef = useRef()
+
+	const decrement = () => setCount((count -= 1))
+	const increment = () => setCount((count += 1))
+
+	const submitRefButton = () => setName(nameRef.current.value)
+
+	useEffect(() => {
+		window.localStorage.setItem('name', name)
+	}, [name])
+
 	return (
-		<div className="background">
-			<h1>Hello React App</h1>
-			<p>you can try every thing</p>
-			<button onClick={() => setOpen(!open)}>try to click</button>
-			{open ? <p>hook opend</p> : <p>hook closed</p>}
+		<div className="App">
+			<p data-testid="countvalue">{count}</p>
+			<button data-testid="decrementButton" type="button" onClick={decrement}>
+				-
+			</button>
+			<button data-testid="incrementButton" type="button" onClick={increment}>
+				+
+			</button>
+			<div>
+				<h2>Testing useRef</h2>
+				<p data-testid="nameValue">{name}</p>
+				<input data-testid="inputName" ref={nameRef} type="text" />
+				<button data-testid="submitRefButton" type="button" onClick={submitRefButton}>
+					Submit
+				</button>
+			</div>
 		</div>
 	)
 }
